8c8f9a0d41315f6645f73d2b11713cd79e42157c
Qiusheng Wu <giswqs@gmail.com>
2022-11-01 08:45:57 -0400

    Added Python 3.10

--- {{cookiecutter.github_repo_name}}/setup.py
+++ {{cookiecutter.github_repo_name}}/setup.py
@@ -47,6 +47,7 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
     ],
     description="{{ cookiecutter.project_short_description }}",
     {%- if 'no' not in cookiecutter.command_line_interface|lower %}
